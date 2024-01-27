9e0d55664ce4e4309b30d21359a7d0788c7d699a
arjunstein <arjun.gnw09@gmail.com>
2024-01-27 14:02:46 +0700

    ✨ feat: Update navbar links based on user role.

--- resources/views/layouts/navbar-welcome.blade.php
+++ resources/views/layouts/navbar-welcome.blade.php
@@ -1,36 +1,52 @@
 <div class="container">
-  <div class="navbar-header">
-    <a href="/" class="navbar-brand"><b>Futsal</b>APP</a>
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-      <i class="fa fa-bars"></i>
-    </button>
-  </div>
+    <div class="navbar-header">
+        <a href="/" class="navbar-brand"><b>Futsal</b>APP</a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <i class="fa fa-bars"></i>
+        </button>
+    </div>
 
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
-    <ul class="nav navbar-nav">
-      <li><a href="#">Lapangan</a></li>
-      <li><a href="#">Futsal</a></li>
-      <li><a href="#">Mini Soccer</a></li>
-      <li><a href="#">Events</a></li>
-    </ul>
-  </div>
-  <!-- /.navbar-collapse -->
-  <!-- Navbar Right Menu -->
-  <div class="navbar-custom-menu">
-    <ul class="nav navbar-nav">
-        @if (Route::has('login'))
-        @auth
-            <li><a href="/backend/dashboard">Home</a></li>
-            @else
-            <li><a href="/login">Login</a></li>
-            @if (Route::has('register'))
-            <li><a href="/register">Register</a></li>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+        <ul class="nav navbar-nav">
+            @if (Route::has('login'))
+                @auth
+                    @if (\Auth::user()->roles == 'admin')
+                        <li><a href="/backend/lapangan">Lapangan</a></li>
+                        <li><a href="/backend/futsal">Futsal</a></li>
+                        <li><a href="/backend/mini_soccer">Mini Soccer</a></li>
+                        <li><a href="/backend/events">Events</a></li>
+                    @else
+                        <li><a href="/lapangan">Lapangan</a></li>
+                        <li><a href="/futsal">Futsal</a></li>
+                        <li><a href="/mini_soccer">Mini Soccer</a></li>
+                        <li><a href="/events">Events</a></li>
+                    @endif
+                @else
+                    <li><a href="/lapangan">Lapangan</a></li>
+                    <li><a href="/futsal">Futsal</a></li>
+                    <li><a href="/mini_soccer">Mini Soccer</a></li>
+                    <li><a href="/events">Events</a></li>
+                @endauth
             @endif
-        @endauth
-        @endif
-    </ul>
-  </div>
-  <!-- /.navbar-custom-menu -->
+        </ul>
+    </div>
+    <!-- /.navbar-collapse -->
+    <!-- Navbar Right Menu -->
+    <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+            @if (Route::has('login'))
+                @auth
+                    <li><a href="/backend/dashboard">Home</a></li>
+                @else
+                    <li><a href="/login">Login</a></li>
+                    @if (Route::has('register'))
+                        <li><a href="/register">Register</a></li>
+                    @endif
+                @endauth
+            @endif
+        </ul>
+    </div>
+    <!-- /.navbar-custom-menu -->
 </div>
 <!-- /.container-fluid -->
